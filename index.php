d7656d7974661f894df11d28be18a810f452a92d
RudolphOosthuizen <50637581+RudolphOosthuizen@users.noreply.github.com>
2020-10-08 02:11:14 +0200

    nuwe cv

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <!-- css -->
+        <link href="css/style.css" rel="stylesheet"/>
+        <!-- Font Awesome -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+        <title>Rudolph Oosthuizen CV</title>
+
+    </head>
+
+    <body class="body-padding">
+        <!-- <div class="loader"></div> -->
+
+        <!-- background animation -->
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <!-- Heading & index -->
+        <div class="row border border-info div-padding myBG">
+            <div class="col-2 text=left">
+                <img src="img/myGesig.PNG">
+            </div>
+            <div class="col-10">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>
+                            Rudolph Oosthuizen
+                        </h2>
+                        <h3>
+                            Curriculum Vitae
+                        </h3>
+                    </div>
+                </div>
+                <div class="row">
+                <div class="col-8 text-right">
+                    <div>
+                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                            <div class="btn-group mr-2" role="group" aria-label="First group">
+                                <button type="button" class="btn btn-secondary">Personal Information</button>
+                                <button type="button" class="btn btn-secondary">iets</button>
+                                <button type="button" class="btn btn-secondary">iets</button>
+                                <button type="button" class="btn btn-secondary">iets</button>
+                                <button type="button" class="btn btn-secondary">iets</button>
+                                <button type="button" class="btn btn-secondary">iets</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 text-right">
+                    <p>
+                        <i class="far fa-envelope"></i></i> rudolphoosthuizen42@gmail.com<br>
+                        <i class="fas fa-mobile-alt"></i> +27 60 838 9684<br>
+                        <i class="fas fa-home"></i> 50 Francois Street<br>
+                        Miederpark, Potchefstroom<br>
+                        2527
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- About Me -->
+        <div class="row border border-info div-padding myBG">
+            Hi. my name is Rudolph Oosthuizen and I am a learning developer looking for a way to get some experience.
+        </div>
+            
+
+        <script>
+            console.log('JS is working');
+
+            function displayLoader(){
+                <div class="loader"></div>
+            }
+
+            funtion hideLoader(){
+                document.getElementById('loader').hidden = true;
+            }
+
+        </script>
+
+        <!-- Custom JS -->
+        <script src="script.js"></script>  
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+                                   
+    </body>
+</html>
