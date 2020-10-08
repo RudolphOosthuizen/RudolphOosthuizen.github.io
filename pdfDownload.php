68181f4e54188611e7a02edd41573da3d21eead0
RudolphOosthuizen <50637581+RudolphOosthuizen@users.noreply.github.com>
2020-10-08 12:39:11 +0200

    pdf fix maybe

--- pdfDownload.php
+++ pdfDownload.php
@@ -1,7 +1,11 @@
 <?php
 
+// opens and closes new tab fast to download CV.pdf from repo
 header("Content-Disposition: attachment; filename=Rudolph_Oosthuizen_CV.pdf");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Content-type: application/pdf;");
 
+//clear output buffer or smth
+ob_clean();
+flush();
 readfile("cvPDF/Rudolph_Oosthuizen_CV.pdf");
